f5b843d60be830e64b228f5c64f42f3e420483a4
yamcha <dbals9812@naver.com>
2019-04-15 20:41:54 +0900

    added riot api url

--- src/main/java/org/cnu/realcoding/riot_crawling/riot_crawling/domain/CurrentWeather.java
+++ src/main/java/org/cnu/realcoding/riot_crawling/riot_crawling/domain/CurrentWeather.java
@@ -5,27 +5,5 @@ import lombok.Data;
 @Data
 public class CurrentWeather {
     private String name;
-    private Main main;
-    private Wind wind;
-    private Others sys;
-
-    @Data
-    public static class Main {
-        private double temp;
-        private int pressure;
-        private int humidity;
-    }
-
-    @Data
-    public static class Wind {
-        private double speed;
-        private int deg;
-    }
-
-    @Data
-    public static class Others {
-        private String country;
-        private long sunrise;
-        private long sunset;
-    }
+    private String id;
 }
